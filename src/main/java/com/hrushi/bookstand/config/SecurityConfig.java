2bae62cb0e7317ce0537090dfa2f2f426fb5f32a
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-11-02 22:44:17 +0530

    add temp user

---
+++ src/main/java/com/hrushi/bookstand/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.hrushi.bookstand.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService inMemoryUserDetailsManager(PasswordEncoder passwordEncoder) {
+        UserDetails temp = User.builder().username("temp").password(passwordEncoder.encode("123")).build();
+        return new InMemoryUserDetailsManager(temp);
+    }
+}
