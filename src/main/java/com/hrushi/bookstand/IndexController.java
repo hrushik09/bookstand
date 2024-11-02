53285115e52cf33850eb725f6eb3618c71258ae6
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-11-02 22:28:20 +0530

    add thymeleaf layout and index page

---
+++ src/main/java/com/hrushi/bookstand/IndexController.java
@@ -0,0 +1,12 @@
+package com.hrushi.bookstand;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class IndexController {
+    @GetMapping("")
+    String index() {
+        return "index";
+    }
+}
