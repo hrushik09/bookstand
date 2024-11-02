197bd1f91a9fcc10fcff6a4b45d8924ebf9e94eb
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-11-02 22:11:30 +0530

    init

---
+++ src/main/java/com/hrushi/bookstand/BookstandApplication.java
@@ -0,0 +1,13 @@
+package com.hrushi.bookstand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstandApplication.class, args);
+    }
+
+}
