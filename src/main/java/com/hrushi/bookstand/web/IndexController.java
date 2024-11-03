7cb3fd586b11bf9ca9ff72b997afa047d2842602
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-11-03 11:04:34 +0530

    add missing annotation

--- src/main/java/com/hrushi/bookstand/web/IndexController.java
+++ src/main/java/com/hrushi/bookstand/web/IndexController.java
@@ -1,11 +1,13 @@
 package com.hrushi.bookstand.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 class IndexController {
+    @GetMapping
     String index() {
         return "index";
     }
