9f8fd3ed769357ce8663effee4337f3097d9299a
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-11-04 08:03:56 +0530

    add missing reference actions

--- src/main/resources/db/migration/V1.3__create_users_authorities.sql
+++ src/main/resources/db/migration/V1.3__create_users_authorities.sql
@@ -4,6 +4,6 @@ CREATE TABLE users_authorities
     user_id      BIGINT NOT NULL,
     authority_id BIGINT NOT NULL,
     CONSTRAINT PK_users_authorities PRIMARY KEY (id),
-    CONSTRAINT FK_users_authorities_user_id FOREIGN KEY (user_id) references users (id),
-    CONSTRAINT FK_users_authorities_authority_id FOREIGN KEY (authority_id) references authorities (id)
+    CONSTRAINT FK_users_authorities_user_id FOREIGN KEY (user_id) references users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_users_authorities_authority_id FOREIGN KEY (authority_id) references authorities (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
