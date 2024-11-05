69ffa6e25481c8065ee3d2fb195e7f58757a8aaa
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-11-05 22:33:44 +0530

    use htmx for dynamic update of profile

--- src/main/java/com/hrushi/bookstand/web/ProfileController.java
+++ src/main/java/com/hrushi/bookstand/web/ProfileController.java
@@ -6,12 +6,11 @@ import com.hrushi.bookstand.domain.profiles.UpdateProfileCommand;
 import com.hrushi.bookstand.domain.profiles.UpdateProfileRequest;
 import com.hrushi.bookstand.domain.users.SecuredUser;
 import jakarta.validation.Valid;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,13 +26,22 @@ class ProfileController {
     String profiles(Model model, @AuthenticationPrincipal SecuredUser securedUser) {
         Profile profile = profileService.findProfile(securedUser.id());
         model.addAttribute("profile", profile);
-        return "profiles";
+        return "profiles/index";
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    String updateProfile(@Valid UpdateProfileRequest request, @AuthenticationPrincipal SecuredUser securedUser) {
+    @GetMapping("/update")
+    String edit(Model model, @AuthenticationPrincipal SecuredUser securedUser) {
+        Profile profile = profileService.findProfile(securedUser.id());
+        model.addAttribute("profile", profile);
+        return "profiles/update";
+    }
+
+    @PutMapping
+    String updateProfile(@Valid UpdateProfileRequest request, @AuthenticationPrincipal SecuredUser securedUser, Model model) {
         UpdateProfileCommand cmd = new UpdateProfileCommand(securedUser.id(), request.firstName(), request.lastName(), request.country(), request.email(), request.bio());
         profileService.updateProfile(cmd);
-        return "redirect:/profiles";
+        Profile profile = profileService.findProfile(securedUser.id());
+        model.addAttribute("profile", profile);
+        return "profiles/profilecard";
     }
 }
