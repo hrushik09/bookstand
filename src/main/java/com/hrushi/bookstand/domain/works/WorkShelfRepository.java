45ac62e7e0ee0bd5db1224f623f2fb8eb536e212
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-11-16 17:56:31 +0530

    add WorkShelfEntity

---
+++ src/main/java/com/hrushi/bookstand/domain/works/WorkShelfRepository.java
@@ -0,0 +1,6 @@
+package com.hrushi.bookstand.domain.works;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface WorkShelfRepository extends JpaRepository<WorkShelfEntity, Long> {
+}
