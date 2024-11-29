d37f10aae2a85bec4619db5dff64920989866fb4
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-11-29 23:33:52 +0530

    remove dummy message

--- src/main/java/com/hrushi/bookstand/web/IndexController.java
+++ src/main/java/com/hrushi/bookstand/web/IndexController.java
@@ -3,15 +3,11 @@ package com.hrushi.bookstand.web;
 import com.hrushi.bookstand.domain.users.SecuredUser;
 import com.hrushi.bookstand.domain.works.AllShelves;
 import com.hrushi.bookstand.domain.works.WorkService;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.text.NumberFormat;
-import java.util.Locale;
 
 @Controller
 @RequestMapping("/")
@@ -26,10 +22,6 @@ class IndexController {
     String index(Model model, @AuthenticationPrincipal SecuredUser securedUser) {
         AllShelves allShelves = workService.getAllShelves(securedUser.id());
         model.addAttribute("allShelves", allShelves);
-        Locale locale = LocaleContextHolder.getLocale();
-        NumberFormat numberFormat = NumberFormat.getNumberInstance(locale);
-        String format = numberFormat.format(15678.43);
-        model.addAttribute("localeExample", format);
         return "index";
     }
 }
